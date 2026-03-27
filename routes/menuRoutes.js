dc694e4e5f2b174a367053d7516ee84741e098e2
Tommy Hulten <tommy.hulten@gmail.com>
2026-03-27 17:16:51 +0100

    grundkod menuRoutes

---
+++ routes/menuRoutes.js
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import db from "../data/db.js";
+
+const router = Router();
+
+// User Story 1
+// GET http://localhost:3000/api/menu-Hämta hela menyn
+
+router.get("/", (req, res) => {
+  const menu = db.prepare("SELECT * FROM menu").all();
+
+  if (!menu || menu.length === 0) {
+    return res.status(404).json({ error: "Menyn hittades inte" });
+  }
+
+  res.json(menu);
+});
+
+export default router;
+
+// GET /api/menu/:id-Hämta en specifik kaffesort
+
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const coffee = db.prepare("SELECT * FROM menu WHERE id = ?").get(id);
+
+  if (!coffee) {
+    return res.status(404).json({ error: "Kaffesorten hittades inte" });
+  }
+  res.json(coffee);
+});
