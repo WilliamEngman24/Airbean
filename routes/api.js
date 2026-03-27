c62c94cce178d7109e26761660933d0accf95616
Tommy Hulten <tommy.hulten@gmail.com>
2026-03-27 17:47:33 +0100

    tog bort kommentar import menuRoutes

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ import { requireApiKey } from "../middleware/requireApiKey.js";
 import userRoutes from "./userRoutes.js";
 
 // remove '//' when files for these routes are created
-//import menuRoutes from "./menuRoutes.js";
+import menuRoutes from "./menuRoutes.js";
 //import orderRoutes from "./orderRoutes.js";
 
 const router = Router ();
