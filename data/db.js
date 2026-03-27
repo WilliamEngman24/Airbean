6bf426d291126f85720f884189745598d64e388e
William Engman <williamengman24@gmail.com>
2026-03-27 15:59:44 +0100

    added tables for menu, orders and order_items. also changed createdAt in users to user_date

--- data/db.js
+++ data/db.js
@@ -3,12 +3,37 @@ import Database from 'better-sqlite3';
 const db = new Database('./data/airbean.db');
 
 db.exec(`
-CREATE TABLE IF NOT EXISTS users (
+  CREATE TABLE IF NOT EXISTS users (
   id TEXT PRIMARY KEY,
   username TEXT NOT NULL,
   email TEXT NOT NULL,
-  createdAt TEXT
-)
+  order_date TEXT
+  );
+
+  CREATE TABLE IF NOT EXISTS menu (
+  id TEXT PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT,
+  price REAL NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS orders (
+  id TEXT PRIMARY KEY,
+  user_id TEXT NOT NULL,
+  total_price REAL NOT NULL,
+  ETA TEXT,
+  order_date TEXT,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  );
+
+  CREATE TABLE IF NOT EXISTS order_items (
+  id TEXT PRIMARY KEY,
+  order_id TEXT NOT NULL,
+  product_id TEXT NOT NULL,
+  quantity INTEGER NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders(id),
+  FOREIGN KEY (product_id) REFERENCES menu(id)
+  );
   `);
 
 export default db;
