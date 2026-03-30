744aa4e9d370f0c8b72cab2b61f665d514e65ef5
William Engman <williamengman24@gmail.com>
2026-03-30 12:52:52 +0200

    added user.json and updated the gitignore

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,15 @@ import { Router } from "express";
 import { requireApiKey } from "../middleware/requireApiKey.js";
 import userRoutes from "./userRoutes.js";
 
+// remove '//' when files for these routes are created
+import menuRoutes from "./menuRoutes.js";
+//import orderRoutes from "./orderRoutes.js";
+
 const router = Router ();
 
 router.use(requireApiKey);
 router.use("/users", userRoutes);
+router.use("/menu", menuRoutes);
+router.use("/orders", orderRoutes);
 
 export default router;
