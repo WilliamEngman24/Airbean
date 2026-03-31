dd0b4b03a516deb139607a0466e6608f70871fd1
Fluuffyyy <Mozoke95@gmail.com>
2026-03-31 10:42:16 +0200

    Test för middleware är utkommenterad i api.js för att testa middleware

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { requireApiKey } from "../middleware/requireApiKey.js";
 import userRoutes from "./userRoutes.js";
+import express from 'express';
+import { validateOrder } from '../middleware/validateOrder.js';
+
 
 // remove '//' when files for these routes are created
 import menuRoutes from "./menuRoutes.js";
@@ -8,11 +11,19 @@ import menuRoutes from "./menuRoutes.js";
 
 const router = Router ();
 
+router.post('/test-order', validateOrder, (req, res) => {
+  res.status(200).json({
+    message: 'Validering OK',
+    items: req.validatedItems
+  });
+});
+
 router.use(requireApiKey);
 //router.use("/users", userRoutes);
 router.use("/menu", menuRoutes);
 
 // get rid of the '//' when file for orderRoutes.js iscreated
 //router.use("/orders", orderRoutes);
+
 
 export default router;
